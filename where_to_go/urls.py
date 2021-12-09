e1124b06fba58aa7f6a6a022154d74d148e0b79e
Alex <soyvitaly@gmail.com>
2021-12-09 23:10:19 +0100

    Added get_place_json

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from where_to_go.views import index
+from where_to_go.views import index, places
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('places/<int:post_id>/', places),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.http.response import JsonResponse
+from django.shortcuts import render, get_list_or_404
 from django.template import loader
-from places.models import Place
+from places.models import Place, Image
 
 moscow_legends = Place.objects.get(placeId='moscow_legends')
 roofs24 = Place.objects.get(placeId='roofs24')
@@ -34,8 +35,30 @@ places_geojson = {
         }
     ]
 }
-
+def get_place_json(place):
+    images = get_list_or_404(Image, place=place)
+    place_json = {
+        "title": place.title,
+        'img': [item.image.url for item in images],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.lng,
+            'lat': place.lat
+        }
+    }
+    return place_json
 
 def index(request):
     return render(request, 'index.html', {'places_geojson': places_geojson})
+
+
+
+def places(request, post_id):
+    try:
+        place = Place.objects.get(pk=post_id)
+    except Place.DoesNotExist:
+        raise Http404("No MyModel matches the given query.")
+    return JsonResponse(get_place_json(place), safe=False,
+                        json_dumps_params={'ensure_ascii': False, 'indent': 2})
 
